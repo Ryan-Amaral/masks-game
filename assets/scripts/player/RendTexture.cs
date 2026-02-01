60335673130afe16b727a22a1fa7be3aa1ae08e3
Athirah Afwina <athirahafwina@gmail.com>
2026-01-31 21:44:00 -0400

    update inventory bar icons

---
+++ assets/scripts/player/RendTexture.cs
@@ -0,0 +1,32 @@
+using Godot;
+using System;
+
+public partial class RendTexture : Godot.TextureRect
+{
+	[Export] private SubViewport Viewport;
+	[Export] private Node3D ModelNode;
+
+	private Tween tween;
+	public override void _Ready()
+	{
+		Texture = Viewport.GetTexture();
+	}
+	
+	public void Highlight()
+	{
+		tween = CreateTween();
+		tween.SetLoops();
+		tween.TweenProperty(ModelNode, "position:y", -0.1, 1.0).SetTrans(Tween.TransitionType.Sine)
+			.SetEase(Tween.EaseType.InOut);
+		tween.TweenProperty(ModelNode, "position:y", 0.1, 1.0).SetTrans(Tween.TransitionType.Sine)
+			.SetEase(Tween.EaseType.InOut);
+
+	}
+
+	public void Deselect()
+	{
+		tween.Stop();
+		ModelNode.Position = Vector3.Zero;
+	}
+	
+}
